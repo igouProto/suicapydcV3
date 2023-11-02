599a70b1215e26325ee5003d7e1215c958932b93
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-01 17:35:05 -0700

    extension reloading messages

--- Replies/Messages.py
+++ Replies/Messages.py
@@ -1,8 +1,13 @@
-'''
+"""
 Base language module in Traditional Chinese
-'''
-class Messages:
+"""
 
+
+class Messages:
     # Generic system messages
     KEYWORD_REPLY_ENABLED = "已為**{}**開啟關鍵字回覆。"
     KEYWORD_REPLY_DISABLED = "已為**{}**關閉關鍵字回覆。"
+
+    # Admin messages
+    EXTENSION_RELOADING = "正在重新載入所有模組..."
+    EXTENSION_RELOADED = "已重新載入所有模組。"
