321a4c104e26bc5d3d73d1cf28662f1b2ec87135
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-02 22:59:48 -0700

    added messages for bot login

--- Replies/Messages.py
+++ Replies/Messages.py
@@ -9,5 +9,6 @@ class Messages:
     KEYWORD_REPLY_DISABLED = "已為**{}**關閉關鍵字回覆。"
 
     # Admin messages
-    EXTENSION_RELOADING = "正在重新載入所有模組..."
-    EXTENSION_RELOADED = "已重新載入所有模組。"
+    EXTENSION_RELOADING = ":warning: 正在重新載入所有模組..."
+    EXTENSION_RELOADED = "✅ 已重新載入所有模組。"
+    BOT_BOOTED = "✅ Suica已登入。"
