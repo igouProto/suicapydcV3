709a0bd85b45d76b4c4e7df759cee29574c68dc1
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-05 20:21:43 -0800

    about

---
+++ Extensions/About.py
@@ -0,0 +1,37 @@
+import logging
+import discord
+from discord.ext import commands
+from Replies.Messages import Messages
+import Suica
+
+# logging
+log = logging.getLogger(__name__)
+
+"""
+About the bot. I'll just hardcode the embed here.
+"""
+
+
+class About(commands.Cog):
+    def __init__(self, bot: Suica.Bot):
+        self.bot = bot
+
+    @commands.command(name="about")
+    async def about(self, ctx):
+        
+        # retrieve bot info
+        bot_info = await self.bot.application_info()
+        bot_id = bot_info.id
+        name = bot_info.name
+        owner = bot_info.owner.mention
+        borntime = "2018-08-01 05:47:51.880000"
+
+        embed = discord.Embed(title='**S**ugoi **U**ltra **I**ntelligent **C**hat **A**ssistant, SUICA', colour=discord.Color.orange())
+        embed.set_author(name=Messages.ABOUT_TITLE, icon_url=self.bot.user.avatar.url)
+        embed.description = Messages.ABOUT.format(bot_id, name, owner, borntime)
+        embed.set_footer(text=Messages.ABOUT_FOOTER.format(self.bot.version))
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(About(bot))
+    log.info("Module loaded")
