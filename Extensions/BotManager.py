82749bb6ffdabe1006213ffd61dec57c0204f1a4
igouProto <48474345+igouProto@users.noreply.github.com>
2023-11-05 20:22:00 -0800

    restart and shutdown (tentative)

--- Extensions/BotManager.py
+++ Extensions/BotManager.py
@@ -1,7 +1,10 @@
 import logging
+import os
+import sys
 import discord
 from discord.ext import commands
 from Replies.Messages import Messages
+import Suica
 
 # logging
 log = logging.getLogger(__name__)
@@ -12,7 +15,7 @@ Bot manager.
 
 
 class BotManager(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Suica.Bot):
         self.bot = bot
         self.bot.remove_command("help")
 
@@ -26,7 +29,8 @@ class BotManager(commands.Cog):
         )
         """
         await self.bot.change_presence(
-            activity=discord.CustomActivity(self.bot.status_message)
+            activity=discord.CustomActivity(self.bot.status_message),
+            status=discord.Status.online
         )
 
     @commands.Cog.listener()
@@ -74,6 +78,20 @@ class BotManager(commands.Cog):
                 Messages.EXTENSION_RELOAD_FAILED.format(f"```{failed_list}```")
             )
 
+    # restart the bot
+    @commands.is_owner()
+    @commands.command(name="restart")
+    async def _restart(self, ctx):
+        await ctx.send(Messages.BOT_RESTARTING)
+        # await self.bot.close()
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    
+    # shutdown the bot (temporarily here cuz the restart command does not play well with discord's login system)
+    @commands.is_owner()
+    @commands.command(name="shutdown")
+    async def _shutdown(self, ctx):
+        await ctx.send("Shutting down...")
+        await self.bot.close()
 
 async def setup(bot):
     await bot.add_cog(BotManager(bot))
