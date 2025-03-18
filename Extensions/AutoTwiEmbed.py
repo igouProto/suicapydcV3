a0e86463b2016c62d9e1a8ee417315e5b7f559ea
igouProto <48474345+igouProto@users.noreply.github.com>
2025-03-18 16:29:20 -0700

    improved twitter embedding + configurable timeout

--- Extensions/AutoTwiEmbed.py
+++ Extensions/AutoTwiEmbed.py
@@ -15,6 +15,7 @@ Scans for twitter links, and if there's no embed, reply with a vxtwitter link.
 class AutoTwiEmbed(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.timeout = 500  # ms
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -31,18 +32,45 @@ class AutoTwiEmbed(commands.Cog):
         suffix = message.content.split(".com/")[1]
 
         # if we don't see images from the embed, reply with a vxtwitter link
+
+        await asyncio.sleep(
+            self.timeout / 1000
+        )  # allow some (configurable, but not permanent atm) time for embeds to come in
         
-        await asyncio.sleep(0.5)  # allow some time for embeds to come in
-        # reply when: there's no embed / no image or image url is present
+        # reply when: there's no embed / no image or image url is present / embed with no img and desc
         # new condition: no image from twitter's photo blob storage is present
-        if (
-            not message.embeds
-            or len(message.embeds) == 0
-            or not message.embeds[0].image.url
-            or not message.embeds[0].image.url.startswith('https://pbs.twimg.com/') # omg elon stop changing stuff...
-        ):
+
+        # if there's no embed at all we also append one
+        if not message.embeds or len(message.embeds) == 0:
             link = "https://fxtwitter.com/" + suffix
             await message.reply(link, mention_author=False)
+            return
+
+        embed = message.embeds[0]
+        desc = embed.description
+
+        print(embed, desc)
+
+        # if there's an embed without desc AND image, we append one too
+        if not desc and (
+            not embed.image.url
+            or not embed.image.url.startswith("https://pbs.twimg.com/")
+        ):  # omg elon stop changing stuff...
+            link = "https://fxtwitter.com/" + suffix
+            await message.reply(link, mention_author=False)
+
+    # configurable timeout
+    # sometimes the embeds do take a longer while to come in
+    @commands.command(name="settwitimeout", aliases=["twito"])
+    @commands.is_owner()
+    async def _set_twit_timeout(self, ctx, timeout: int = None):
+
+        if not timeout:
+            await ctx.send(Messages.TWI_TIMEOUT_CURRENT.format(self.timeout))
+            return
+
+        self.timeout = timeout
+        await ctx.send(Messages.TWI_TIMEOUT_SET.format(timeout))
 
 
 async def setup(bot):
